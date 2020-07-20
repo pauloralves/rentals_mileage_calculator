f6ea8dfe2174200e8c8f236a330390094c36d8a3
pauloralves <prafpaulinho@gmail.com>
2020-07-20 19:22:38 -0400

    Correct double 'size' call

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -7,7 +7,7 @@ class Rental < ApplicationRecord
 	end
 
 	def processed_csv_files
-		self.csv_files.select{ |f| f.status == 2 }.size
+		self.csv_files.select{ |f| f.status == 2 }
 	end
 
 	def average_mileage_km
